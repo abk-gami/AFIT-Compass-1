65c74c64a3e6e2206ec7fb9ffd23e923a344382e
abk-gami <yahabubakar01@gmail.com>
2023-08-29 13:39:49 +0100

    brr

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 
   import { StyleSheet, View } from 'react-native'
-// import Test from './test';
+import Test from './test';
 import MapScreen from './MapScreen';
+import Hehe from './hehe';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <MapScreen/>
       {/* <Test/> */}
+      {/* <Hehe /> */}
     </View>
   );
 }
