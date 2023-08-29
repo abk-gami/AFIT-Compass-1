1b6a56837fa85229979013117e2fa6b31145ecb3
abk-gami <yahabubakar01@gmail.com>
2023-08-29 10:29:36 +0100

    jiggy

--- config.js
+++ config.js
@@ -20,3 +20,26 @@ const firebaseConfig = {
   export { firebase };
 
   
+
+  // import { StyleSheet, View } from 'react-native'
+// // import Test from './test';
+// import MapScreen from './MapScreen';
+
+// const App = () => {
+//   return (
+//     <View style={styles.container}>
+//       <MapScreen/>
+//       {/* <Test/> */}
+//     </View>
+//   );
+// }
+
+// export default App
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+
+// })
+
