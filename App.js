bcfe34468a701b291d9636366957dfa45af70927
abk-gami <yahabubakar01@gmail.com>
2023-09-01 01:51:45 +0100

    chii

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 
-  import { StyleSheet, View, SafeAreaView } from 'react-native'
+  import { StyleSheet, View, SafeAreaView, StatusBar } from 'react-native'
 import Test from './test';
 import MapScreen from './MapScreen';
 import Hehe from './hehe';
+import BottomSheet from './bot';
 
 const App = () => {
   return (
+    // <View style={styles.main}>
     <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#001b7c" barStyle="light-content"/>
       <MapScreen/>
       {/* <Test/> */}
       {/* <Hehe /> */}
+      {/* <BottomSheet /> */}
     </SafeAreaView>
+    // </View>
   );
 }
 
@@ -19,6 +24,7 @@ export default App
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // marginTop: 37,
+    // backgroundColor: '#fff'
   },
-
 })
