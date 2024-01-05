1df183ca4b8ca6220786af9625002809e0ad0034
Abk <127848306+abk-gami@users.noreply.github.com>
2024-01-05 22:59:43 +0100

    Update LastOne.js

--- LastOne.js
+++ LastOne.js
@@ -63,7 +63,7 @@ const ExploreScreen = () => {
     markers,
     categories: [
       { 
-        name: 'Fastfood Cener', 
+        name: 'Fastfood Ceer', 
         icon: <MaterialCommunityIcons style={styles.chipsIcon} name="food-fork-drink" size={18} />,
       },
       {
